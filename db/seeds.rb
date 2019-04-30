a940920f44de6f501f6f9ca0658c96d84e52002c
ElleeB <laura.trager@gmail.com>
2019-04-30 10:37:03 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "avery", email: "avery@gmail.com", bio: "do you know the muffin man, the muffin man, the muffin man? Do you know, the muffin man, who lives on Drury lane?", password: "muffin", password_confirmation: "muffin")
+
+categories = Category.create([{name: 'Toys'}, {name: 'Furniture'}, {name: 'Instruments'}])
+
+collections = Collection.create([
+  {title: "hotwheels", user_id: 1, category_id: 1, description: "lots of little cars!"},
+  {title: "chairs", user_id: 1, category_id: 2, description: "dining, accent, decorative!"},
+  {title: "guitars", user_id: 1, category_id: 3, description: "pluck pluck pluck and strum strum strum!"}
+])
+
+items = Item.create([
+  {name: "camaro", collection_id: 1, description: "red with minor scratches. acquired two years ago with my dad."},
+  {name: "porsche", collection_id: 1, description: "black in mint condition. this is my favorite."},
+  {name: "jeep", collection_id: 1, description: "grey and orange. model year 2010."},
+
+  {name: "eames", collection_id: 2, description: "mint condition black leather."},
+  {name: "rocking", collection_id: 2, description: "maple rocker authentic 1866."},
+  {name: "morris", collection_id: 2, description: "brown leather on oak. slight wear on leather. authentic 1879."},
+
+  {name: "fender strat", collection_id: 3, description: "gold and black body slight wear. acquired in 1988. authentic 1959."},
+  {name: "fender tele", collection_id: 3, description: "telecaster in mint condition."},
+  {name: "les paul", collection_id: 3, description: "limited edition blue body."}
+])
