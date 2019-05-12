8784139eff9134f741a3dfece6d08d6467e21385
ElleeB <laura.trager@gmail.com>
2019-05-12 12:40:40 -0400

    Add client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import CollectionsContainer from './containers/CollectionsContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        COLLEKT!
+      </header>
+      <CollectionsContainer />
+    </div>
+  );
+}
+
+export default App
---
+++ client/src/containers/CollectionsContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Component } from 'react'
+// import './App.css'
+
+class CollectionsContainer extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        Collections Container!
+      </div>
+    );
+  }
+}
+
+export default CollectionsContainer
